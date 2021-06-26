c2a9b0fcca79fe9846fbc81d8b13e1a07c024987
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2021-06-26 15:18:31 +1000

    Create band_name_generator.py
    Band Name Generator is a simple python program that asks your name, greets you in that name and then asks a couple of questions to help you think of a new band name.

---
+++ band_name_generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: zero_kelvin
+# @Date:   2021-06-26 15:12:34
+# @Last Modified by:   zero_kelvin
+# @Last Modified time: 2021-06-26 15:14:14
+
+print("Hello " + input("What is your name? \n") + ". It's a pleasure to meet you!")
+print("Welcome to the Band Name Generator...")
+home = input("Where did you grow up? \n")
+pet = input("What was the name of a pet? \n")
+print("The name of your band could be " + home + " " + pet)
