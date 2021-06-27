cfd721c82ea5cec24bf662e27000472383325b30
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2021-06-27 20:34:16 +1000

    Create bmi-calculator.py
    BMI calculator that tells you what weight range you are in.

---
+++ bmi-calculator.py
@@ -0,0 +1,19 @@
+
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+weight_as_float = float(weight)
+height_as_float = float(height)
+bmi = round(weight_as_float / (height_as_float ** 2))
+
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}. You are considered underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}. You are within a normal weight range.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}. You are overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}. You are obese.")
+elif bmi > 35:
+  print(f"DAMN. Your BMI is {bmi}. Step away from the cupcake. You are clinically obese.")
