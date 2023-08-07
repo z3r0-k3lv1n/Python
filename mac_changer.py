e34eacb2c222df22694b0b177d313c932c7c9efe
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 13:32:12 +1000

    Update mac_changer.py
    Removed shell=True from subprocess.call() to reduce threat of exploitation.

--- mac_changer.py
+++ mac_changer.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-# Written in Python3
+# Written for Python3
 
 import subprocess
 
 
 def main():
 
-    interface = "Interface Name > "
-    new_mac = input("MAC Address > ")
+    interface = input("Interface > ")
+    new_mac = input("New MAC > ")
     print("=" * 80)
-    print(f"[+]    Changing MAC address for {interface} to {new_mac}...")
+    print(f"[+]    Changing MAC address for {interface} to {new_mac}")
     print("=" * 80)
-    
-    subprocess.call('ifconfig {interface} down', shell=True)
-    subprocess.call('ifconfig {interface} hw ether {new_mac}', shell=True)
-    subprocess.call('ifconfig {interface} up', shell=True)
+
+    subprocess.call(["ifconfig", interface, "down"]) 
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
 
 if __name__ == '__main__':
