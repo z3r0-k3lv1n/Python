0104f7c759edcaec9aba12d585e63044a8d68c1c
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 12:51:05 +1000

    Update mac_changer.py
    Changed interface and mac address to variables to allow for greater scalability.

--- mac_changer.py
+++ mac_changer.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+# Written in Python3
 
 import subprocess
 
 
 def main():
-    subprocess.call('clear', shell=True)
+
+    interface = "wlan0"
+    new_mac = "00:11:22::33::44:55"
+    print("[+]" * 60)
+    print(f"[+]    Changing MAC address for {interface} to {new_mac}...")
+    print("[+]" * 60)
+    
     subprocess.call('ifconfig', shell=True)
-    subprocess.call('ifconfig wlan0 down', shell=True)
-    subprocess.call('ifconfig wlan0 hw ether 00:11:22:33:44:55', shell=True)
-    subprocess.call('ifconfig wlan0 up', shell=True)
-    subprocess.call('clear', shell=True)
-    subprocess.call('ifconfig', shell=True)
+    subprocess.call('ifconfig {interface} down', shell=True)
+    subprocess.call('ifconfig {interface} hw ether {new_mac}', shell=True)
+    subprocess.call('ifconfig {interface} up', shell=True)
 
 
 if __name__ == '__main__':
