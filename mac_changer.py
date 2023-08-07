822a6d247f98e6d1d7284d73ce29d6c8b94cbccb
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 16:10:17 +1000

    Update mac_changer.py
    Added more detailed notes.
    Added ability to pass arguments from the command line with the values to be stored as variables to be then passed into the subprocess.call() function.

--- mac_changer.py
+++ mac_changer.py
@@ -9,15 +9,19 @@ def main():
     # ================================================================================================================
     # Allow arguments to be passed in the command line when initiating the program
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="The interface to have its MAC address changed.")
-    parser.parse_args()
+    parser.add_option("-i", "--interface", dest="INTERFACE", help="The interface to have its MAC address changed.")
+    parser.add_option("-m", "--mac", dest="NEW_MAC", help="The new MAC address for the chosen interface.")
+    options, arguments = parser.parse_args()  # Sets two new variables and passes the values of the arguments that
+    # the user passes in the command line into them. Options will contain the values while arguments contain
+    # the -i or -m as an example
     # ================================================================================================================
 
     # ================================================================================================================
     # Set a banner and allow the user to enter their inputs for the interface name to be changed and the new MAC
     # address
-    interface = input("Interface > ")
-    new_mac = input("New MAC > ")
+    interface = options.INTERFACE  # Sets the interface variable as the value of the interface argument as passed from
+    # the command line
+    new_mac = options.NEW_MAC
     print("=" * 80)
     print(f"[+]    Changing MAC address for {interface} to {new_mac}")
     print("=" * 80)
