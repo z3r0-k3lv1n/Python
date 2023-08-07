2d699860deed337a4297b6accf6ecf7e96711ded
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 12:53:06 +1000

    Update mac_changer.py
    Small change to banner heading

--- mac_changer.py
+++ mac_changer.py
@@ -8,9 +8,9 @@ def main():
 
     interface = "wlan0"
     new_mac = "00:11:22::33::44:55"
-    print("[+]" * 60)
+    print("=" * 80)
     print(f"[+]    Changing MAC address for {interface} to {new_mac}...")
-    print("[+]" * 60)
+    print("=" * 80)
     
     subprocess.call('ifconfig', shell=True)
     subprocess.call('ifconfig {interface} down', shell=True)
