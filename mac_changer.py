470688ab4c5730b10a58ae668cbd91369bff2dc2
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 17:52:55 +1000

    Update mac_changer.py
    Added error handling to the get_arguments() function.
    Error messages if no arguments and values are passed in at the command line.

--- mac_changer.py
+++ mac_changer.py
@@ -10,7 +10,14 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="The interface to have its MAC address changed.")
     parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address for the chosen interface.")
-    return parser.parse_args()
+    options, arguments = parser.parse_args()
+    if not options.interface:
+        # Code to handle error if options.interface does not hold a value
+        parser.error("[-]   Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        # Code to handle error if options.new_mac does not hold a value
+        parser.error("[-]   Please specify a new mac address, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -33,7 +40,7 @@ def change_mac(interface, new_mac):
 
 
 def main():
-    options, arguments = get_arguments()  # Sets the options and arguments variables as the returned value of 
+    options = get_arguments()  # Sets the options and arguments variables as the returned value of
     # the get_arguments() function
     change_mac(options.interface, options.new_mac)  # Calls the change_mac() function and passes in the values of
     # the arguments entered on the command line by the user that are parsed in the options and arguments variables
