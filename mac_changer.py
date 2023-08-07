2d6718e0d8defce40843a2808f99891dcc9c3b91
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 16:31:00 +1000

    Update mac_changer.py
    Created a new function called change_mac() that houses the commands for changing the mac address and prints an explanatory banner when the program is run.
    Calls the function in the main function and passes in the values of the options variable to the new function.

--- mac_changer.py
+++ mac_changer.py
@@ -5,23 +5,11 @@ import subprocess
 import optparse
 
 
-def main():
-    # ================================================================================================================
-    # Allow arguments to be passed in the command line when initiating the program
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="INTERFACE", help="The interface to have its MAC address changed.")
-    parser.add_option("-m", "--mac", dest="NEW_MAC", help="The new MAC address for the chosen interface.")
-    options, arguments = parser.parse_args()  # Sets two new variables and passes the values of the arguments that
-    # the user passes in the command line into them. Options will contain the values while arguments contain
-    # the -i or -m as an example
-    # ================================================================================================================
-
+def change_mac(interface, new_mac):
     # ================================================================================================================
     # Set a banner and allow the user to enter their inputs for the interface name to be changed and the new MAC
     # address
-    interface = options.INTERFACE  # Sets the interface variable as the value of the interface argument as passed from
-    # the command line
-    new_mac = options.NEW_MAC
+    # ================================================================================================================
     print("=" * 80)
     print(f"[+]    Changing MAC address for {interface} to {new_mac}")
     print("=" * 80)
@@ -34,6 +22,21 @@ def main():
     # single command string
     subprocess.call(["ifconfig", interface, "up"])  # Calls the "ifconfig" and adds the "interface" user
     # input and the "up" command as a single command string
+
+
+def main():
+    # ================================================================================================================
+    # Allow arguments to be passed in the command line when initiating the program
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="The interface to have its MAC address changed.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address for the chosen interface.")
+    options, arguments = parser.parse_args()  # Sets two new variables and passes the values of the arguments that
+    # the user passes in the command line into them. Options will contain the values while arguments contain
+    # the -i or -m as an example
+    # ===============================================================================================================
+
+    change_mac(options.interface, options.new_mac)  # Calls the change_mac() function and passes in the valies of
+    # the arguments entered on the command line by the user that are parsed in the options and arguments variables
 
 
 if __name__ == '__main__':
