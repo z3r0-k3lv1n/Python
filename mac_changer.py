074cd6ac193fc82690492a97801df09c8aee2d86
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-07 14:28:09 +1000

    Update mac_changer.py
    Added additional comments to explain sections of the code.
    Added the ability to pass arguments in the command line when initiating the program.

--- mac_changer.py
+++ mac_changer.py
@@ -2,19 +2,34 @@
 # Written for Python3
 
 import subprocess
+import optparse
 
 
 def main():
+    # ================================================================================================================
+    # Allow arguments to be passed in the command line when initiating the program
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="The interface to have its MAC address changed.")
+    parser.parse_args()
+    # ================================================================================================================
 
+    # ================================================================================================================
+    # Set a banner and allow the user to enter their inputs for the interface name to be changed and the new MAC
+    # address
     interface = input("Interface > ")
     new_mac = input("New MAC > ")
     print("=" * 80)
     print(f"[+]    Changing MAC address for {interface} to {new_mac}")
     print("=" * 80)
 
-    subprocess.call(["ifconfig", interface, "down"]) # Calls the "ifconfig" and adds the "interface" user input and the "down" command as a single command string
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    # ================================================================================================================
+    subprocess.call(["ifconfig", interface, "down"])  # Calls the "ifconfig" and adds the "interface" user
+    # input and the "down" command as a single command string
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])  # Calls the "ifconfig" and adds the "interface"
+    # user input and the "hw" and "ether" commands and passes in the user input for the new mac address as a
+    # single command string
+    subprocess.call(["ifconfig", interface, "up"])  # Calls the "ifconfig" and adds the "interface" user
+    # input and the "up" command as a single command string
 
 
 if __name__ == '__main__':
